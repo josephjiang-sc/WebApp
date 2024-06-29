49262f02fd20b8164e7c3fbcb37f36c04973aab5
Joseph Jiang <joseph.jiang@skycity.co.nz>
2024-06-30 11:56:42 +1200

    Routing middleware

---
+++ WebApp/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public string Index() // Action Method
+        {
+            return "Hello from Action Method";
+        }
+        public string Error() // Action Method
+        {
+            return "Error";
+        }
+    }
+}
--- WebApp/Program.cs
+++ WebApp/Program.cs
@@ -2,9 +2,16 @@ using System.Net.Mime;
 using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllersWithViews();
+
 var app = builder.Build();
 
-app.MapGet("/", (HttpContext context) => {
-});
+app.UseRouting();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}"
+    );
 
 app.Run();
