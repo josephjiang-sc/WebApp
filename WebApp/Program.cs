fd5a5c48ea47def7ecadb8f498be9c9b3ad6ed21
Joseph Jiang <joseph.jiang@skycity.co.nz>
2024-06-30 11:48:00 +1200

    Updates

--- WebApp/Program.cs
+++ WebApp/Program.cs
@@ -5,19 +5,6 @@ var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
 app.MapGet("/", (HttpContext context) => {
-    string html = @"
-    <html>
-        <h1>Hello World</h1>
-    </html>
-    ";
-    WriteHtml(context, html);
 });
 
 app.Run();
-
-void WriteHtml(HttpContext context, string html)
-{
-    context.Response.ContentType = MediaTypeNames.Text.Html;
-    context.Response.ContentLength = Encoding.UTF8.GetByteCount(html);
-    context.Response.WriteAsync(html);
-}
